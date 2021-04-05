2c875e16bc4cd1186df14ab01df4cffa5886fd88
Treidex <treidexsemextetry@gmail.com>
2021-04-05 11:10:37 -0700

    Window

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,35 @@
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
 #include <cstdio>
 
-int main(int nArgs, char *args[])
+int main()
 {
-    std::puts("Hello, world!");
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+        return 1;
+    
+    SDL_Window *wnd = SDL_CreateWindow("We Don't", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 0, 0, SDL_WINDOW_SHOWN | SDL_WINDOW_FULLSCREEN);
+    SDL_Surface *surface = SDL_GetWindowSurface(wnd);
+
+    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0x00, 0xFF, 0x00));
+    SDL_UpdateWindowSurface(wnd);
+
+    bool running = true;
+    while (running)
+    {
+        SDL_Event e;
+        while (SDL_PollEvent(&e))
+            switch (e.type)
+            {
+            case SDL_QUIT:
+                running = false;
+                break;
+            case SDL_KEYDOWN:
+                if (e.key.keysym.sym == SDLK_ESCAPE)
+                    running = false;
+                break;
+            }
+    }
+    
+    SDL_DestroyWindow(wnd);
+    SDL_Quit();
 }
